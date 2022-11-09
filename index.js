89b0271b4007619facf13cfaca4128a3d1697fc9
Ivan Onofriichuk <ivanonofrijcuk@gmail.com>
2022-11-09 23:31:57 +0200

    Випадкова генерація квадрату

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const start = document.querySelector('#start')
 const game = document.querySelector('#game')
+
+let score = 0
 start.addEventListener('click', startGame)
 game.addEventListener('click', handleBoxClick)
 
@@ -11,18 +13,34 @@ function startGame() {
 }
 
 function handleBoxClick(event) {
-console.log(event.target.dataset)
+if (event.target.dataset){
+	score++
+	renderBox()
+}
 }
 
 function renderBox(){
+const boxSize = getRandom(30, 100)
+game.innerHTML = ''
 const box = document.createElement('div')
-box.style.height = box.style.width = '50px'
+const gameSize = game.getBoundingClientRect()
+const maxTop = gameSize.height - boxSize
+const maxLeft = gameSize.width - boxSize
+//console.log(gameSize)
+
+
+box.style.height = box.style.width = boxSize + 'px'
 box.style.position = 'absolute'
 box.style.backgroundColor = '#000'
-box.style.top = '50px'
-box.style.left = '70px'
+box.style.top = getRandom(0, maxTop) + 'px'
+box.style.left = getRandom(0, maxLeft) + 'px'
 box.style.cursor = 'pointer'
 box.setAttribute('data-box', 'true')
 
 game.insertAdjacentElement('afterbegin', box)
+}
+
+
+function getRandom(min, max){
+	return Math.floor(Math.random() * (max - min) + min)
 }
